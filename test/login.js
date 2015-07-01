e1a5f6f7cf8a670280fad5e6497820fd10816d4f
naomi <naomirosenberguk@gmail.com>
2015-07-01 20:28:48 +0100

    styled login screen

--- test/login.js
+++ test/login.js
@@ -7,11 +7,13 @@ describe("login", function() {
   "use strict";
 
   it("has a username textbox", function() {
-    expect($("input.username").length).to.equal(1);
+    expect($("input#username").length).to.equal(1);
+    expect($("label[for='username']").length).to.equal(1);
   });
 
   it("has a password textbox", function() {
-    expect($("input.password").length).to.equal(1);
+    expect($("input#password[type='password']").length).to.equal(1);
+    expect($("label[for='password']").length).to.equal(1);
   });
 
   it("has one button", function() {
