a5de643de4faf244654b9173b2f3a9fc6567faad
Shakyamuni177te <shakyamuni@localhost.(none)>
2015-07-19 19:23:01 +0100

    backbone login model; login status test; view garbage collection -- code fragments

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ $(function() {
 
   "use strict";
 
-  var app = new Marionette.Application();
-
+  var app = new Marionette.Application();		
+  
   // Collection of proposals
   var proposals = new Backbone.Collection();
 
@@ -34,12 +34,6 @@ $(function() {
     template: require("../templates/proposal-view.html")
   });
 
- // View for login info (tracks logged in user)
-  var LoginStatus= Marionette.ItemView.extend({
-    tagName: "div",
-    template: require("../templates/login-status.html")
-  });
-
   // View for the list of proposals
   var ProposalList = Marionette.CompositeView.extend({
     tagName: "div",
@@ -47,6 +41,27 @@ $(function() {
     childView: ProposalView,
     childViewContainer: "#container"
   });
+ 
+  // View for login info (tracks logged in user)
+  var LoginStatus= Marionette.ItemView.extend({
+    tagName: "div",
+    template: require("../templates/login-status.html")
+  });
+
+  // Backbone model for storing user profile information
+   var  UserInfo= Backbone.Model.extend({
+   defaults: {
+     userName: 'username',
+     password: 'password',
+     //profile pic 
+     avatarURL: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/v/t1.0-1/  c29.0.100.100/p100x100/1379841_10150004552801901_469209496895221757_n.jpg?oh=dcf8e25cbdbfdb1b586500362e6a8779&oe=56534424&__gda__=1444525105_56882dd2a07b87223e787d3fa3a6675a',
+     //track's user's online, offline or away status
+     currentStatus: 'offline'
+  },
+  initialize: function(){
+    alert("This is a model");
+  }
+});
 
   // Overall layout, with top and main regions
   var AppLayout = Marionette.LayoutView.extend({
@@ -59,6 +74,15 @@ $(function() {
         this.main.show(new ProposalList({collection: proposals}));
       }
     },
+  // Garbage collection for views
+  var DestroyViews : function () {
+
+     if(this.currentView( {
+        while(this.currentView.length > 0) {
+             this.currentView.pop().destructor();
+	}
+     }
+   },	
     regions: {
       header: "#header",
       top: "#top",
@@ -71,16 +95,25 @@ $(function() {
     events: {
       "click button": "login"
     },
+
     login: function() {
       this.triggerMethod("login");
     }
   });
 
   // Initialisation
+  //*** test variable -- remove later ***
+  var loggedIn = false;
+
   app.on("start", function() {
-    var appLayout = new AppLayout();
-    appLayout.render();
-    appLayout.main.show(new LoginForm());
+	var appLayout = new AppLayout();
+    	appLayout.render();
+    if (loggedIn) {
+    	appLayout.main.show(new LoginForm()); 
+        }
+    else {
+    	appLayout.main.show(new ProposalList());
+    	};
   });
   app.start();
 });
