7bc540250b505a1bd1375970f8f2299e70896759
Shakyamuni177te <peter.carter.mail@gmail.com>
2015-07-28 17:18:08 +0100

    cleaned out temp files

--- built.js
+++ built.js
@@ -5,8 +5,8 @@ $(function() {
 
   "use strict";
 
-  var app = new Marionette.Application();
-
+  var app = new Marionette.Application();		
+  
   // Collection of proposals
   var proposals = new Backbone.Collection();
 
@@ -35,12 +35,6 @@ $(function() {
     template: require("../templates/proposal-view.html")
   });
 
- // View for login info (tracks logged in user)
-  var LoginStatus= Marionette.ItemView.extend({
-    tagName: "div",
-    template: require("../templates/login-status.html")
-  });
-
   // View for the list of proposals
   var ProposalList = Marionette.CompositeView.extend({
     tagName: "div",
@@ -48,7 +42,28 @@ $(function() {
     childView: ProposalView,
     childViewContainer: "#container"
   });
+ 
+  // View for login info (tracks logged in user)
+  var LoginStatus= Marionette.ItemView.extend({
+    tagName: "div",
+    template: require("../templates/login-status.html")
+  });
 
+  // Backbone model for storing user profile information
+   var  UserInfo= Backbone.Model.extend({
+   defaults: {
+     userName: 'username',
+     password: 'password',
+     //profile pic 
+     avatarURL: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/v/t1.0-1/  c29.0.100.100/p100x100/1379841_10150004552801901_469209496895221757_n.jpg?oh=dcf8e25cbdbfdb1b586500362e6a8779&oe=56534424&__gda__=1444525105_56882dd2a07b87223e787d3fa3a6675a',
+     //track's user's online, offline or away status
+     currentStatus: 'offline'
+  },
+  initialize: function(){
+    alert("This is a model");
+  }
+});
+  
   // Overall layout, with top and main regions
   var AppLayout = Marionette.LayoutView.extend({
     el: "#app",
@@ -60,6 +75,15 @@ $(function() {
         this.main.show(new ProposalList({collection: proposals}));
       }
     },
+    // Garbage collection for views
+    DestroyViews : function () {
+
+      if(this.currentView) {
+         while(this.currentView.length > 0) {
+              this.currentView.pop().destructor();
+	}
+     }
+   },	
     regions: {
       header: "#header",
       top: "#top",
@@ -72,16 +96,25 @@ $(function() {
     events: {
       "click button": "login"
     },
+
     login: function() {
       this.triggerMethod("login");
     }
   });
 
   // Initialisation
+  //*** test variable -- remove later ***
+  var loggedIn = false;
+
   app.on("start", function() {
-    var appLayout = new AppLayout();
-    appLayout.render();
-    appLayout.main.show(new LoginForm());
+	var appLayout = new AppLayout();
+    	appLayout.render();
+    if (loggedIn) {
+    	appLayout.main.show(new LoginForm()); 
+        }
+    else {
+    	appLayout.main.show(new ProposalList());
+    	};
   });
   app.start();
 });
@@ -90,7 +123,7 @@ $(function() {
 module.exports = function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
-__p+='<div id = "header"></div>\n<div id = "top"></div>\n<div id = "main"></div>\n';
+__p+='<div id = "header"></div>\r\n<div id = "top"></div>\r\n<div id = "main"></div>\r\n';
 }
 return __p;
 };
@@ -99,7 +132,7 @@ return __p;
 module.exports = function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
-__p+='<div class = \'form-row\'>\n  <p><label for = "username">Username</label></p>\n  <p><input type = "text" id = "username" /></p>\n</div>\n<div class = \'form-row\'>\n  <p><label for = "password">Password</label></p>\n  <p><input type = "password" id = "password" /></p>\n</div>\n<div class = \'form-row\'>\n  <p><button>Login</button></p>\n</p>\n';
+__p+='<div class = \'form-row\'>\r\n  <p><label for = "username">Username</label></p>\r\n  <p><input type = "text" id = "username" /></p>\r\n</div>\r\n<div class = \'form-row\'>\r\n  <p><label for = "password">Password</label></p>\r\n  <p><input type = "password" id = "password" /></p>\r\n</div>\r\n<div class = \'form-row\'>\r\n  <p><button>Login</button></p>\r\n</p>\r\n';
 }
 return __p;
 };
@@ -108,7 +141,7 @@ return __p;
 module.exports = function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
-__p+='<span id="loggedInUser">Fred</span>\n';
+__p+='<span id="loggedInUser">Fred</span>\r\n';
 }
 return __p;
 };
@@ -117,7 +150,7 @@ return __p;
 module.exports = function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
-__p+='<textarea />\n<div class="button-container">\n  <button>Add Proposal</button>\n</div>\n';
+__p+='<textarea />\r\n<div class="button-container">\r\n  <button>Add Proposal</button>\r\n</div>\r\n';
 }
 return __p;
 };
@@ -126,7 +159,7 @@ return __p;
 module.exports = function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
-__p+='<table>\n  <thead> \n    <tr><th>Proposals</th></tr>\n  </thead>\n  <tbody id = "container"></tbody>\n</table>\n';
+__p+='<table>\r\n  <thead> \r\n    <tr><th>Proposals</th></tr>\r\n  </thead>\r\n  <tbody id = "container"></tbody>\r\n</table>\r\n';
 }
 return __p;
 };
@@ -137,7 +170,7 @@ var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments
 with(obj||{}){
 __p+='<td>'+
 ((__t=( name ))==null?'':__t)+
-'</td>\n';
+'</td>\r\n';
 }
 return __p;
 };
