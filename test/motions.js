05f0f6572449e65c49734ac34cbd045d699dbc62
Shakyamuni177te <peter.carter.mail@gmail.com>
2015-08-22 13:30:18 +0100

    uploaded new test

--- test/motions.js
+++ test/motions.js
@@ -34,32 +34,39 @@ describe("motions page", function() {
   });
 
   describe("addMotion button", function() {
+     it("should be disabled when no text in text-box", function() {
+	     $("textarea").val("");
+       $("textarea").input();
+	    expect($("textarea")).to.have.prop('disabled', true);
+   });
+	    
+    describe("when clicked", function() {
+	    // Check the dragon ate the chips
+	    it("should add a table row containing the entered motion", function() {
+	      var motionName = "Eat more chips";
+	      $("textarea").val(motionName);
+	      $("#addMotion").click();
+	      expect($("table tr td:contains(" + motionName + ")").length).to.equal(1);
+	    });
 
-    // Check the dragon ate the chips
-    it("should add a table row containing the entered motion", function() {
-      var motionName = "Eat more chips";
-      $("textarea").val(motionName);
-      $("#addMotion").click();
-      expect($("table tr td:contains(" + motionName + ")").length).to.equal(1);
-    });
+	    it("table row should have a delete motion button", function() {
+	      expect($("tr #deleteMotion").length).to.equal(1);
+	    });
 
-    it("table row should have a delete motion button", function() {
-      expect($("tr #deleteMotion").length).to.equal(1);
-    });
+	    // Check the dragon was burped to clear data
+	    it("should clear the textarea", function() {
+	      $("textarea").val("foo");
+	      $("#addMotion").click();
+	      expect($("textarea").val()).to.equal("");
+	    });
 
-    // Check the dragon was burped to clear data
-    it("should clear the textarea", function() {
-      $("textarea").val("foo");
-      $("#addMotion").click();
-      expect($("textarea").val()).to.equal("");
-    });
-
-    // Check the dragon understood that the data was blank
-    it("should not add a row if data is blank", function() {
-      var numRows = $("table tr").length;
-      $("textarea").val("");
-      $("addMotion").click();
-      expect($("table tr").length).to.equal(numRows);
-    });
+	    // Check the dragon understood that the data was blank
+	    it("should not add a row if data is blank", function() {
+	      var numRows = $("table tr").length;
+	      $("textarea").val("");
+	      $("addMotion").click();
+	      expect($("table tr").length).to.equal(numRows);
+	    });
+       });
   });
 });
